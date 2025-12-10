76910dafdea416262546e021a1e8a6202be8794d
FedericoCanale <icocanale@gmail.com>
2025-12-10 13:38:36 +0100

    add complete controller for index

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,13 +1,26 @@
-// Rotta index: GET http://localhost:3000/movies : lista di tutti i film
+// Index: GET http://localhost:3000/movies : lista di tutti i film
+
+const db = require('../database/db');
 const index = (req, res) => {
-    res.json({
-        message: 'Qui ci sarà la lista dei film'
+    const sql = 'SELECT * FROM movies';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error('Errore nella query dei film:', err);
+            return res.status(500).json({
+                error: true,
+                message: 'Errore interno del server'
+            });
+        }
+
+        res.json(results);
     });
 };
 
-// Rotta show: GET http://localhost:3000/movies/1 : dettaglio film
+// Show: GET http://localhost:3000/movies/1 : dettaglio film
 const show = (req, res) => {
     const { id } = req.params;
+
 
     res.json({
         message: `Qui ci saranno i dettagli del film con id ${id}`

0ea861d2bd1bf84d24cba7f82975596e9ffe2b59
FedericoCanale <icocanale@gmail.com>
2025-12-11 11:34:36 +0100

    add cors to my express app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const port = 3000;
 const db = require('./database/db');
 app.use(express.static("public"));
@@ -21,7 +22,10 @@ app.use(notFound);
 // Middleware gestione errori 
 const errorHandler = require('./middlewares/errorHandler');
 app.use(errorHandler);
-
+//Middleware cors
+app.use(cors({
+    origin: `http://localhost:5173`
+}));
 // Avvio del server
 app.listen(port, () => {
     console.log(`Server in ascolto su http://localhost:${port}`);

6791e4c4908123f2837133210bd5b2c8c65116cd
FedericoCanale <icocanale@gmail.com>
2025-12-11 13:53:24 +0100

    modify middleware cors location, now connecting to react

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const db = require('./database/db');
 app.use(express.static("public"));
 app.use(express.json());
 
+//Middleware cors
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
+
 // Rotta di test
 app.get("/", (req, res) => {
     res.send("Homepage del blog di film");
@@ -22,12 +29,7 @@ app.use(notFound);
 // Middleware gestione errori 
 const errorHandler = require('./middlewares/errorHandler');
 app.use(errorHandler);
-//Middleware cors
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+
 // Avvio del server
 app.listen(port, () => {
     console.log(`Server in ascolto su http://localhost:${port}`);
